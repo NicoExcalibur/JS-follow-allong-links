4369dbaf570f129ae5c1b8839a96b421fe1f7789
NicoExcalibur <nico.zanz@gmail.com>
2021-07-01 11:34:48 +0200

    highlight links with coordonates

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ highlight.classList.add("highlight");
 document.body.append(highlight);
 
 function highlightLink() {
-  console.log("Highlight");
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  highlight.style.width = `${linkCoords.width}px`;
+  highlight.style.height = `${linkCoords.height}px`;
+  highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
 }
 
 triggers.forEach((a) => a.addEventListener("mouseenter", highlightLink));
